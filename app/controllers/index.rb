218db8f7d2beeabcc1b8ba759152980a445afccd
DineshRai <drai89@gmail.com>
2014-06-13 11:45:59 -0400

    restful routs add

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,53 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+post '/login' do
+  @email = params[:email]
+  @password = params[:password]
+  if user
+    session[:user_id] = user.id
+    @surveys = Survey.all
+    redirect '/sruvey/list'
+  end
+end
+
+
+post '/signup' do
+  @user = User.new (email: params[:email], password: params[:password])
+  if @user.save
+    session[:user_id] = @user.id
+    @surveys = Survey.all
+    redirect '/survey/list'
+end
+
+get '/survey/new' do
+	erb :new
+end
+
+post '/survey' do
+	survey = Survey.create(name: params[:title], user_id: session[:user_id])
+	# question_length = params[:questions].length
+	params[:questions].each do |question|
+		question = Question.create(name: question[:name], survey_id: survey.id )
+		index = params[:questions].index(question)
+		params[:options][index].each do |option|
+			Option.create(name: option[:name], question_id: question.id)
+		end
+	end
+	redirect '/survey/list'
+end
+
+get '/survey/list' do
+	@surveys = Survey.all
+	erb :list
+end
+
+get '/survey/:id/answer' do
+	@survey = Survey.find(params[:id])
+	erb :answer
+end
+
+post '/survey/:id' do
+	
+end
